f47991196018396c6301d49d1cef80934ef45f53
TMA-2 <tma2@live.com>
2025-04-03 06:56:49 -0500

    - GuidEx: Fixed Convert-BinaryLSB. Added some Pester tests.

--- Private/Convert-BinaryLSB.ps1
+++ Private/Convert-BinaryLSB.ps1
@@ -1,13 +1,14 @@
 function Convert-BinaryLSB {
-    [OutputType(ParameterSetName = 'Byte', [byte[]])]
+    # [OutputType(ParameterSetName = 'Byte', [byte])]
+    [OutputType(ParameterSetName = 'Bytes', [byte[]])]
     [OutputType(ParameterSetName = 'Guid', [Guid])]
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = 'Bytes')]
     param (
         [Parameter(
             Mandatory,
             Position = 0,
             ValueFromPipeline,
-            ParameterSetName = 'Byte'
+            ParameterSetName = 'Bytes'
         )]
         [byte[]]$Bytes,
         [Parameter(
@@ -32,29 +33,38 @@ function Convert-BinaryLSB {
             $Byte1Fmt = [System.Convert]::ToString($Byte1, 2).PadLeft(8, '0')
             $Byte2Fmt = [System.Convert]::ToString($Byte2, 2).PadLeft(8, '0')
 
-            '{0}: {1:X2} -> {2:X2}; 0b{3} -> 0b{4}' -f $Step, $Byte1, $Byte2, $Byte1Fmt, $Byte2Fmt
+            '{0}: 0x{1:X2} -> 0x{2:X2}; 0b{3} -> 0b{4}' -f $Step, $Byte1, $Byte2, $Byte1Fmt, $Byte2Fmt
         }
     }
 
     process {
         if ($ParamSet -eq 'Guid') {
             $Bytes = $Guid.ToByteArray()
+            <# if($LITTLE_ENDIAN) {
+                $Bytes = [byte[]]$Bytes[3..0] + [byte[]]$Bytes[5..4] + [byte[]]$Bytes[7..6] + [byte[]]$Bytes[8..15]
+            } #>
         }
 
-        $reversedBytes = @()
+        $bytecount = 0
+        $reversedBytes = [byte[]]::new($Bytes.Length)
+
         foreach ($byte in $bytes) {
             $reversedByte = [byte]0
-            for ($i = 0; $i -lt 8; $i++) {
-                # ex: 0xF8 -> 0x8F
+            <# for ($i = 0; $i -lt 8; $i++) {
                 $reversedByte = $reversedByte -bor (($byte -shr $i -band 1) -shl (7 - $i))
-                Show-Step -Step $i -Byte1 $byte -Byte2 $reversedByte | Write-Verbose
-            }
-            $reversedBytes += $reversedByte
+            } #>
+            $LeftBits = $byte -shl 4 -band 0xF0
+            $RightBits = $byte -shr 4 -band 0x0F
+            $reversedByte = $LeftBits -bor $RightBits
+            
+            Show-Step -Step $bytecount -Byte1 $byte -Byte2 $reversedByte | Write-Verbose
+            $reversedBytes[$bytecount] = $reversedByte
+            $bytecount++
         }
 
         if ($ParamSet -eq 'Guid') {
-            return [Guid]::NewGuid($reversedBytes)
-        } elseif ($ParamSet -eq 'Byte') {
+            return [Guid]::new($reversedBytes)
+        } elseif ($ParamSet -eq 'Bytes') {
             return $reversedBytes
         }
     }
--- Test/New-UUID.Tests.ps1
+++ Test/New-UUID.Tests.ps1
@@ -1,5 +1,7 @@
 BeforeAll {
-    . "$PSScriptRoot\..\New-UUID.ps1"
+    . "$PSScriptRoot\..\Private\Convert-BinaryLSB.ps1"
+    . "$PSScriptRoot\..\Public\Convert-UUIDBytes.ps1"
+    . "$PSScriptRoot\..\Public\New-UUID.ps1"
 }
 
 Describe "New-UUID Function" {
@@ -12,5 +14,41 @@ Describe "New-UUID Function" {
 
         # Assert
         $result | Should -Be $expectedGUID
+    }
+}
+
+Describe "GUID Support Functions" {
+    Context "Convert-UUIDBytes" {
+        It "Should return a GUID" {
+            $guid = Convert-UUIDBytes "{01020304-0506-0708-090a-0b0c0d0e0f10}"
+            $guid | Should -BeOfType [guid]
+        }
+        It "Should swap each byte order" {
+            $guid = Convert-UUIDBytes "{01020304-0506-0708-090a-0b0c0d0e0f10}"
+            $guid.ToString() | Should -Be "04030201-0605-0807-090a-0b0c0d0e0f10"
+        }
+        It "Should swap the byte order as well as endianness" {
+            $guid = Convert-UUIDBytes "{6D690465-8FAF-4B5F-8DA8-9177EED149CE}" -Reverse
+            $guid.ToString() | Should -Be "564096D6-FAF8-F5B4-D88A-1977EE1D94EC"
+        }
+    }
+    Context "Convert-BinaryLSB" {
+        It "Should return a byte" {
+            $byte = Convert-BinaryLSB 0xA1
+            $byte | Should -BeOfType [byte]
+        }
+        It "Should swap the byte order" {
+            $byte = Convert-BinaryLSB [byte[]]@(0x10, 0x20, 0x30, 0x40)
+            $byte | Should -Be (0x01, 0x02, 0x03, 0x04)
+        }
+        It "Should take pipeline input" {
+            [guid]"{906F89D8-00DA-3583-8057-A7425FB9FDD4}" | Convert-BinaryLSB | Should -Exist
+        }
+        It "Should reverse a GUID's byte order" {
+            $GUIDA = [guid]"{906F89D8-00DA-3583-8057-A7425FB9FDD4}"
+            $GUIDB = Convert-BinaryLSB $GUIDA
+            $GUIDB | Should -BeOfType [guid]
+            $GUIDB | Should -Be [guid]"D4FD9F25-7A04-5780-83DA-89F90600D8A1"
+        }
     }
 }
